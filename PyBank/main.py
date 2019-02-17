ecd5b4392e6638d70c4dcf24a90ff9b54a24fcd8
annabl38 <47066898+annabl38@users.noreply.github.com>
2019-02-16 23:01:46 -0500

    "Adding code "

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,54 @@
 import os
 import csv
 csvpath=os.path.join("Resources", "budget_data.csv")
+
+total_months=0
+profits=[]
+changes=[]
+oldrow=867884
+greatchange=0
+lowchange=0
+
+with open (csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csvheader=next(csvreader)
+
+    for row in csvreader:
+        total_months=total_months+1
+        profits.append(float(row[1]))
+        change=float(row[1])-oldrow
+        changes.append(change)
+        if change > greatchange:
+            greatchange=change
+            greatmonth=row[0]
+        if change < lowchange:
+            lowchange=change
+            lowmonth=row[0]
+        oldrow=float(row[1])
+
+averagechange=round(sum(changes)/(len(changes)-1),2)
+print("Financial Analysis")
+print("-------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${sum(profits)}")
+print(f"Average Change: ${averagechange}")
+print(f"Greatest Increase in Profits: {greatmonth} ({greatchange})")
+print(f"Greatest Decrease in Profits: {lowmonth} ({lowchange})")
+
+txtfile=open("FinAnalysis.txt", "w")
+txtfile.write("Financial Analysis")
+txtfile.write("-------------------")
+txtfile.write(f"Total Months: {total_months}")
+txtfile.write(f"Total: ${sum(profits)}")
+txtfile.write(f"Average Change: ${averagechange}")
+txtfile.write(f"Greatest Increase in Profits: {greatmonth} ({greatchange})")
+txtfile.write(f"Greatest Decrease in Profits: {lowmonth} ({lowchange})")
+
+  #Financial Analysis
+  #----------------------------
+  #Total Months: 86
+  #Total: $38382578
+  #Average  Change: $-2315.12
+  #Greatest Increase in Profits: Feb-2012 ($1926159)
+  #Greatest Decrease in Profits: Sep-2013 ($-2196167)
